18b635bad92276a56ed689025d4b7e07534908bf
Mosquitooo <ljzhou@localhost.localdomain>
2016-02-25 17:53:56 +0800

    modify qsort

--- Gowork/src/sort/Algorithm/qsort/qsort.go
+++ Gowork/src/sort/Algorithm/qsort/qsort.go
@@ -10,21 +10,22 @@ func quicksort(values []int, left int, right int){
 	
 	key := values[left]
 	i,j := left,right
+
 	for i < j {
-		for i<j && values[j] > key{
+		for i<j && values[j] >= key{
 			j--
 		}
 		values[i] = values[j]
 		
-		for i<j && values[i] < key{
+		for i<j && values[i] <= key{
 			i++
 		}
 		values[j] = values[i]
 	}
 	values[i] = key
-
-	//quicksort(values, left, i - 1)
-	//quicksort(values, i+1, right)
+	
+	quicksort(values, left, i - 1)
+	quicksort(values, i+1, right)
 }
 
 func QuickSort(values []int){
